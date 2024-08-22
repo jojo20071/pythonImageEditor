df7728b3f0daedccccd09002430016104b987825
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-08-22 21:21:16 +0200

    1

---
+++ image.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+class ImageEditorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Editor")
+        self.root.geometry("800x600")
+        self.image_label = tk.Label(root)
+        self.image_label.pack()
+        self.menu = tk.Menu(root)
+        root.config(menu=self.menu)
+        self.file_menu = tk.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="Open", command=self.open_image)
+        self.file_menu.add_command(label="Save", command=self.save_image)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=root.quit)
+        self.image = None
+
+    def open_image(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.image = Image.open(file_path)
+            self.display_image()
+
+    def save_image(self):
+        if self.image:
+            file_path = filedialog.asksaveasfilename(defaultextension=".png")
+            if file_path:
+                self.image.save(file_path)
+
+    def display_image(self):
+        img = self.image.resize((600, 400), Image.ANTIALIAS)
+        img_tk = ImageTk.PhotoImage(img)
+        self.image_label.config(image=img_tk)
+        self.image_label.image = img_tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageEditorApp(root)
+    root.mainloop()
